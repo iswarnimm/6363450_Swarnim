67429e64b1bfb137bd487ca43c86dec87776368c
iswarnimm <swarnimsam@gmail.com>
2025-06-29 23:23:19 +0530

    Add files via upload

---
+++ week2_Moq_handson_task1/code/CustomerCommTests/CustomerCommTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommTests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> mockMailSender;
+        private CustomerComm customerComm;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Create a mock object of IMailSender
+            mockMailSender = new Mock<IMailSender>();
+
+            // Setup mock: whenever SendMail is called, return true
+            mockMailSender.Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            // Inject the mock into CustomerComm
+            customerComm = new CustomerComm(mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ReturnsTrue_WhenMailIsMocked()
+        {
+            // Act
+            bool result = customerComm.SendMailToCustomer();
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
---
+++ week2_Moq_handson_task1/code/CustomerCommTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomerCommTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomerCommTests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10ab1ea7-5ed9-46e9-95d5-950f6ca50f58")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
